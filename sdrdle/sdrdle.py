7e15f68e5614c2c458389bfecf2adc85301fa6e1
Clayton Smith <argilo@gmail.com>
2022-08-05 20:42:56 -0400

    Refactor tile-drawing into a separate function

--- sdrdle/sdrdle.py
+++ sdrdle/sdrdle.py
@@ -7,6 +7,9 @@ TILE_SIZE = 70
 FONT_SIZE = 50
 COLORS = (128, 208, 255)
 
+TITLE_FONT = ImageFont.truetype("FreeSansBold.ttf", FONT_SIZE * 3 // 2)
+TILE_FONT = ImageFont.truetype("FreeSansBold.ttf", FONT_SIZE)
+
 WIDTH = TILE_SPACING * 6
 HEIGHT = TILE_SPACING * 9
 
@@ -29,36 +32,37 @@ def score(target, guess):
     return result
 
 
+def draw_tile(draw, center, letter, score):
+    center_x, center_y = center
+
+    rect = [
+        (center_x - TILE_SIZE // 2, center_y - TILE_SIZE // 2),
+        (center_x + TILE_SIZE // 2, center_y + TILE_SIZE // 2)
+    ]
+
+    if letter == " ":
+        draw.rectangle(xy=rect, outline=255, width=4)
+    else:
+        draw.rectangle(xy=rect, fill=COLORS[score], outline=255, width=4)
+        text_x, text_y = draw.textsize(letter, font=TILE_FONT)
+        draw.text((center_x - (text_x // 2), center_y - (text_y // 2)), letter, font=TILE_FONT, fill=0)
+
+
 def draw_board(draw, target, guesses):
     while len(guesses) < 6:
         guesses.append("     ")
 
-    title_font = ImageFont.truetype("FreeSansBold.ttf", FONT_SIZE * 3 // 2)
-    tile_font = ImageFont.truetype("FreeSansBold.ttf", FONT_SIZE)
-
     title = "SDRdle"
-    text_x, text_y = draw.textsize(title, font=title_font)
-    draw.text((WIDTH // 2 - (text_x // 2), TILE_SPACING * 3 // 2 - (text_y // 2)), title, font=title_font, fill=255)
+    text_x, text_y = draw.textsize(title, font=TITLE_FONT)
+    draw.text((WIDTH // 2 - (text_x // 2), TILE_SPACING * 3 // 2 - (text_y // 2)), title, font=TITLE_FONT, fill=255)
 
     for row in range(6):
         center_y = TILE_SPACING * (row + 3)
-        scores = score(target, guesses[row])
+        guess = guesses[row]
+        scores = score(target, guess)
         for col in range(5):
             center_x = (WIDTH // 2) + TILE_SPACING * (col - 2)
-            rect = [
-                (center_x - TILE_SIZE // 2, center_y - TILE_SIZE // 2),
-                (center_x + TILE_SIZE // 2, center_y + TILE_SIZE // 2)
-            ]
-
-            letter = guesses[row][col]
-
-            if letter == " ":
-                draw.rectangle(xy=rect, outline=255, width=4)
-            else:
-                draw.rectangle(xy=rect, fill=COLORS[scores[col]], outline=255, width=4)
-
-            text_x, text_y = draw.textsize(letter, font=tile_font)
-            draw.text((center_x - (text_x // 2), center_y - (text_y // 2)), letter, font=tile_font, fill=0)
+            draw_tile(draw, (center_x, center_y), guess[col], scores[col])
 
 
 image = Image.new(mode="L", size=(WIDTH, HEIGHT), color=0)
