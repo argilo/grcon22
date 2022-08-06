180e0e0d08d73c68879671c49f91ecf3d3ce2990
Clayton Smith <argilo@gmail.com>
2022-08-05 20:33:47 -0400

    Add scoring

--- sdrdle/sdrdle.py
+++ sdrdle/sdrdle.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
-import random
 from PIL import Image, ImageDraw, ImageFont
 
 TILE_SPACING = 80
 TILE_SIZE = 70
 FONT_SIZE = 50
+COLORS = (128, 208, 255)
 
 WIDTH = TILE_SPACING * 6
-HEIGHT = TILE_SPACING * 8
+HEIGHT = TILE_SPACING * 9
+
+
+def score(target, guess):
+    target_letters = list(target)
+    result = [0] * len(target)
+
+    for i in range(len(target)):
+        if guess[i] == target_letters[i]:
+            target_letters[i] = " "
+            result[i] = 2
+
+    for i in range(len(target)):
+        if guess[i] in target_letters:
+            index = target_letters.index(guess[i])
+            target_letters[index] = " "
+            result[i] = 1
+
+    return result
 
 
 def draw_board(draw, target, guesses):
@@ -20,10 +38,11 @@ def draw_board(draw, target, guesses):
 
     title = "SDRdle"
     text_x, text_y = draw.textsize(title, font=title_font)
-    draw.text((WIDTH // 2 - (text_x // 2), TILE_SPACING * 3 // 4 - (text_y // 2)), title, font=title_font, fill=255)
+    draw.text((WIDTH // 2 - (text_x // 2), TILE_SPACING * 3 // 2 - (text_y // 2)), title, font=title_font, fill=255)
 
     for row in range(6):
-        center_y = TILE_SPACING * (row + 2)
+        center_y = TILE_SPACING * (row + 3)
+        scores = score(target, guesses[row])
         for col in range(5):
             center_x = (WIDTH // 2) + TILE_SPACING * (col - 2)
             rect = [
@@ -34,9 +53,9 @@ def draw_board(draw, target, guesses):
             letter = guesses[row][col]
 
             if letter == " ":
-                draw.rectangle(xy=rect, width=4, outline=255)
+                draw.rectangle(xy=rect, outline=255, width=4)
             else:
-                draw.rectangle(xy=rect, fill=255 - random.randrange(3) * 80)
+                draw.rectangle(xy=rect, fill=COLORS[scores[col]], outline=255, width=4)
 
             text_x, text_y = draw.textsize(letter, font=tile_font)
             draw.text((center_x - (text_x // 2), center_y - (text_y // 2)), letter, font=tile_font, fill=0)
