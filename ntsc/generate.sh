1d21405d09d795d431cf9a065b640d88ac1e7902
Clayton Smith <argilo@gmail.com>
2022-08-29 14:42:59 -0400

    Use a bash loop

--- ntsc/generate.sh
+++ ntsc/generate.sh
@@ -5,30 +5,20 @@ set -e
 qrencode -o rickroll.png 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 qrencode -o flag.png 'FLAG{2OBGfE29cR1SpOXuJpI832FyAWvNPLtC}'
 
-convert \
-  grcon22_orig.png \
-  -resize 600x \
-  rickroll.png \
-  -background white \
-  -gravity north \
-  -append \
-  -extent 640x480 \
-  -font Arial \
-  -pointsize 24 \
-  -draw "text 224,432 'VE3IRR/W3'" \
-  grcon22_rickroll.png
-
-convert \
-  grcon22_orig.png \
-  -resize 600x \
-  flag.png \
-  -background white \
-  -gravity north \
-  -append \
-  -extent 640x480 \
-  -font Arial \
-  -pointsize 24 \
-  -draw "text 224,432 'VE3IRR/W3'" \
-  grcon22_flag.png
+for SOURCE in rickroll flag
+do
+  convert \
+    grcon22_orig.png \
+    -resize 600x \
+    ${SOURCE}.png \
+    -background white \
+    -gravity north \
+    -append \
+    -extent 640x480 \
+    -font Arial \
+    -pointsize 24 \
+    -draw "text 224,432 'VE3IRR/W3'" \
+    grcon22_${SOURCE}.png
+done
 
 python3 ntsc_encode.py
