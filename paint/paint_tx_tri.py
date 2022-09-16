71244c6484509aaeb775571b30dc779f71eba393
Clayton Smith <argilo@gmail.com>
2022-09-15 23:53:02 -0400

    Finish "signal to noise" challenge

--- paint/paint_tx_tri.py
+++ paint/paint_tx_tri.py
@@ -6,8 +6,9 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Paint Tx
-# GNU Radio version: v3.11.0.0git-181-g643e1337
+# GNU Radio version: 3.10.4.0-rc1
 
+from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import filter
 from gnuradio.filter import firdes
@@ -34,8 +35,9 @@ class paint_tx(gr.top_block):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 2000000
-        self.resamp_taps = resamp_taps = firdes.low_pass(2.0, samp_rate, (samp_rate / 2)*0.48,(samp_rate / 2)*0.05, window.WIN_HAMMING, 6.76)
+        self.resamp_taps = resamp_taps = firdes.low_pass(8, samp_rate * 8, samp_rate * 0.48,samp_rate * 0.05, window.WIN_HAMMING, 6.76)
         self.rate = rate = 10
+        self.noise_amp = noise_amp = 0
         self.data = data = (0,)*4096
         self.center_freq = center_freq = 923500000
 
@@ -43,8 +45,8 @@ class paint_tx(gr.top_block):
         # Blocks
         ##################################################
         self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=2,
-                decimation=1,
+                interpolation=8,
+                decimation=7,
                 taps=resamp_taps,
                 fractional_bw=0)
         self.pfb_arb_resampler_xxx_0 = pfb.arb_resampler_ccf(
@@ -53,15 +55,19 @@ class paint_tx(gr.top_block):
             flt_size=32)
         self.pfb_arb_resampler_xxx_0.declare_sample_delay(0)
         self.blocks_vector_source_x_0 = blocks.vector_source_c(data, False, 1, [])
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'final.c32', False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'signal_to_noise.sigmf-data', False)
         self.blocks_file_sink_0.set_unbuffered(False)
+        self.blocks_add_xx_0 = blocks.add_vcc(1)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, noise_amp, 0)
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_file_sink_0, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.pfb_arb_resampler_xxx_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.pfb_arb_resampler_xxx_0, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.pfb_arb_resampler_xxx_0, 0))
 
 
@@ -70,7 +76,7 @@ class paint_tx(gr.top_block):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.set_resamp_taps(firdes.low_pass(2.0, self.samp_rate, (self.samp_rate / 2)*0.48, (self.samp_rate / 2)*0.05, window.WIN_HAMMING, 6.76))
+        self.set_resamp_taps(firdes.low_pass(8, self.samp_rate * 8, self.samp_rate * 0.48, self.samp_rate * 0.05, window.WIN_HAMMING, 6.76))
 
     def get_resamp_taps(self):
         return self.resamp_taps
@@ -85,6 +91,13 @@ class paint_tx(gr.top_block):
     def set_rate(self, rate):
         self.rate = rate
         self.pfb_arb_resampler_xxx_0.set_rate(self.rate)
+
+    def get_noise_amp(self):
+        return self.noise_amp
+
+    def set_noise_amp(self, noise_amp):
+        self.noise_amp = noise_amp
+        self.analog_noise_source_x_0.set_amplitude(self.noise_amp)
 
     def get_data(self):
         return self.data
@@ -114,19 +127,20 @@ def main(top_block_cls=paint_tx, options=None):
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    rows = os.path.getsize("rect.c32") // (8 * 4096)
+    rows = os.path.getsize("rect.c32") // (8 * 1024)
     f = open("rect.c32", "rb")
 
     for n in range(rows):
         print(n)
-        row = f.read(8 * 4096)
-        floats = struct.unpack("f" * (2 * 4096), row)
+        row = f.read(8 * 1024)
+        floats = struct.unpack("f" * (2 * 1024), row)
         samples = []
-        for x in range(4096):
+        for x in range(1024):
             samples.append(complex(floats[2*x], floats[2*x+1]))
 
         tb.set_data(samples)
-        tb.set_rate(100**(n / rows))
+        tb.set_rate(200**(n / rows))
+        tb.set_noise_amp(0 if n < 640 else 1.5 * ((n - 640) / (rows - 640)))
         tb.run()
 
     f.close()
